f7dd7aba4d8dc8cdf15d9d73004f4123082d55a1
Alex <alexis1784@gmail.com>
2016-01-29 14:23:47 +0500

    working version

---
+++ EF_lesson_6_6/MobileContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_6_6
+{
+    class MobileContext : DbContext
+    {
+        public MobileContext()
+            : base("EF_lesson_6_6")
+        { }
+        public DbSet<Phone> Phones { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Phone>().Map(m =>
+            {
+                m.Properties(d => new { d.Name, d.Id });
+                m.ToTable("Mobiles");
+            }).Map(m =>
+            {
+                m.Properties(d => new { d.Company, d.Price });
+                m.ToTable("MobilesInfo");
+            }); ;
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ EF_lesson_6_6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_lesson_6_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (MobileContext db = new MobileContext())
+            {
+                Phone p1 = new Phone { Name = "Samsung Galaxy S5", Company = "Samsung", Price = 14000 };
+                Phone p2 = new Phone { Name = "Nokia Lumia 630", Company = "Nokia", Price = 8000 };
+
+                db.Phones.Add(p1);
+                db.Phones.Add(p2);
+                db.SaveChanges();
+
+                foreach (Phone p in db.Phones)
+                    Console.WriteLine("{0} ({1}) - {2}", p.Name, p.Company, p.Price);
+                Console.ReadLine();
+            }
+        }
+    }
+}
